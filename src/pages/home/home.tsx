d3c33a9d662ae5577521771b24374ea7c00d80ff
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-11-09 17:20:14 +0500

    1 line

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -110,9 +110,7 @@ const Home: FunctionComponent<HomeProps> = () => {
               <Typography fontFamily='Mulish' id="modal-modal-title" variant="h6" component="h2">
                 Create a book
               </Typography>
-              <svg cursor='pointer' onClick={handleClose} xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                <path d="M15 9.5L9 15.5M9 9.5L15 15.5M22 12.5C22 18.0228 17.5228 22.5 12 22.5C6.47715 22.5 2 18.0228 2 12.5C2 6.97715 6.47715 2.5 12 2.5C17.5228 2.5 22 6.97715 22 12.5Z" stroke="#151515" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
+              <svg cursor='pointer' onClick={handleClose} xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none"><path d="M15 9.5L9 15.5M9 9.5L15 15.5M22 12.5C22 18.0228 17.5228 22.5 12 22.5C6.47715 22.5 2 18.0228 2 12.5C2 6.97715 6.47715 2.5 12 2.5C17.5228 2.5 22 6.97715 22 12.5Z" stroke="#151515" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" /></svg>
             </Box>
             <TextField onChange={(e) => setIsbn(e.target.value)} variant="standard" type='isbn' placeholder='Enter isbn ...' InputProps={{ disableUnderline: true }} style={{ marginTop: '10px', background: 'white', width: '100%', height: '47px', padding: '10px 16px', border: '1px solid #EBEBEB', borderRadius: '6px', borderColor: 'none' }} />
             <Box display='flex' gap='20px' marginTop='20px'>
