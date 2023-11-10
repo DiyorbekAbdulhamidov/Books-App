304e6437407422eecfc2dc03be98dd3a05539c9a
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-11-10 20:20:56 +0500

    codes

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -78,14 +78,13 @@ const Home: FunctionComponent<HomeProps> = () => {
 
   const handleDeleteBook = (id: number) => {
     setDeleteId(id);
-    axios.delete(`https://0001.uz/books/${id}`, { headers: authHeadersFifth })
+    axios.delete(`https://0001.uz/books/${id}`, { headers : authHeadersFifth })
       .then((response) => {
         alert.success('Successfully deleted!');
         window.location.reload();
       })
       .catch((error) => {
-        alert.error(error.response.data.message);
-        setLoading(false);
+        alert.warning('Please click again')
       });
   }
 
@@ -98,8 +97,7 @@ const Home: FunctionComponent<HomeProps> = () => {
         handleClose();
       })
       .catch((error) => {
-        alert.error(error.response.data.message);
-        setLoading(false);
+        alert.warning('Please click again')
       });
   };
 
@@ -139,7 +137,7 @@ const Home: FunctionComponent<HomeProps> = () => {
 
                   {book.id ?
                     <div className='actions' style={{ display: 'flex', flexDirection: 'column', gap: '5px', marginTop: '40px' }}>
-                      <div key={book?.id} onClick={() => { setDeleteId(book?.id); handleDeleteBook(book?.id); }} style={{ width: '32px', height: '32px', background: '#FF4D4F', borderRadius: '6px 6px 6px 0', cursor: 'pointer', padding: '6px 5px' }}>
+                      <div key={book?.id} onClick={() => { handleDeleteBook(book?.id); }} style={{ width: '32px', height: '32px', background: '#FF4D4F', borderRadius: '6px 6px 6px 0', cursor: 'pointer', padding: '6px 5px' }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="19" viewBox="0 0 17 16" fill="none"><path d="M11.3334 3.99998V3.46665C11.3334 2.71991 11.3334 2.34654 11.1881 2.06133C11.0603 1.81044 10.8563 1.60647 10.6054 1.47864C10.3202 1.33331 9.94682 1.33331 9.20008 1.33331H8.13341C7.38668 1.33331 7.01331 1.33331 6.72809 1.47864C6.47721 1.60647 6.27324 1.81044 6.14541 2.06133C6.00008 2.34654 6.00008 2.71991 6.00008 3.46665V3.99998M7.33341 7.66665V11M10.0001 7.66665V11M2.66675 3.99998H14.6667M13.3334 3.99998V11.4666C13.3334 12.5868 13.3334 13.1468 13.1154 13.5746C12.9237 13.951 12.6177 14.2569 12.2414 14.4487C11.8136 14.6666 11.2535 14.6666 10.1334 14.6666H7.20008C6.07998 14.6666 5.51992 14.6666 5.0921 14.4487C4.71578 14.2569 4.40982 13.951 4.21807 13.5746C4.00008 13.1468 4.00008 12.5868 4.00008 11.4666V3.99998" stroke="#FEFEFE" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" /></svg>
                       </div>
                       <div onClick={() => handleOpenEdit()} style={{ width: '32px', height: '32px', background: '#6200EE', borderRadius: '0px 6px 6px 6px', cursor: 'pointer', padding: '6px 5px' }}>
@@ -160,7 +158,7 @@ const Home: FunctionComponent<HomeProps> = () => {
                     <TextField onChange={(e) => setStatus(e.target.value)} variant="standard" type='number' placeholder='Enter status ...' InputProps={{ disableUnderline: true }} style={{ marginTop: '10px', background: 'white', width: '100%', height: '47px', padding: '10px 16px', border: '1px solid #EBEBEB', borderRadius: '6px', borderColor: 'none' }} />
                     <Box display='flex' gap='20px' marginTop='20px'>
                       <Button onClick={() => handleCloseEdit()} sx={{ width: '200px', bgcolor: 'none', border: 'solid 1px #6200EE', fontSize: '16px', fontFamily: 'Mulish', color: '#6200EE', textTransform: 'none', "&:hover": { background: 'none' } }}>Close</Button>
-                      <Button onClick={() => { handleOpenEdit(); handleEditBook(book.id); setEditId(book.id) }} disabled={loading} sx={{ width: '200px', bgcolor: '#6200EE', fontSize: '16px', fontFamily: 'Mulish', color: '#FEFEFE', textTransform: 'none', "&:hover": { background: '#6200EE' } }}>{loading ? 'Loading...' : 'Submit'}</Button></Box>
+                      <Button onClick={() => { handleOpenEdit(); handleEditBook(book.id); }} disabled={loading} sx={{ width: '200px', bgcolor: '#6200EE', fontSize: '16px', fontFamily: 'Mulish', color: '#FEFEFE', textTransform: 'none', "&:hover": { background: '#6200EE' } }}>{loading ? 'Loading...' : 'Submit'}</Button></Box>
                   </Box>
                 </Modal>
               </Grid>
